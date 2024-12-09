d453287d4810309636f5c31e546e4fdb9c5f6fc9
Avijit Das <avijitavi338895@gmail.com>
2024-12-09 23:58:46 +0600

    Login page complete

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,113 +1,232 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/pages/home_page.dart';
+import 'package:quiz_app/pages/sign_up_page.dart';
+import 'package:quiz_app/widgets/gradient_color.dart';
 import 'package:quiz_app/widgets/text_widgets.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+    bool _obscureText = true;
+
     return Scaffold(
       body: Container(
         margin: EdgeInsets.symmetric(vertical: 40.0, horizontal: 20.0),
         color: Colors.white,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Image.asset(
-                "assets/images/login.png",
-                height: 100,
-                width: 100,
-                fit: BoxFit.cover,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Image.asset(
+                  "assets/images/login.png",
+                  height: 100,
+                  width: 100,
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: " Email ",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 15.0,
-                        fontWeight: FontWeight.bold),
+              SizedBox(
+                height: 20.0,
+              ),
+              RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: " Email ",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text: "*",
+                      style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  if (!RegExp(
+                          r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
+                      .hasMatch(value)) {
+                    return 'Please enter a valid email address';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  hintText: "avi@gmail.com",
+                  hintStyle: AppTextStyle.labelStyle(),
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    borderSide: BorderSide.none,
                   ),
-                  TextSpan(
-                    text: "*",
+                  contentPadding: const EdgeInsets.only(
+                    top: 20.0,
+                    left: 16.0,
+                    right: 16.0,
+                    bottom: 8.0,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: " Password ",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text: "*",
+                      style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              TextFormField(
+                obscureText: _obscureText,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+
+                  if (value.length < 6) {
+                    return 'Password must be at least 6 characters';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  hintStyle: AppTextStyle.labelStyle(),
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding: const EdgeInsets.only(
+                    top: 20.0,
+                    left: 16.0,
+                    right: 16.0,
+                    bottom: 8.0,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  "Forgot Password?",
+                  style: TextStyle(
+                    fontSize: 13.0,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.black54,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              GestureDetector(
+                onTap: () {
+                  if (_formKey.currentState?.validate() ?? false) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => HomePage(),
+                      ),
+                    );
+                  }
+                },
+                child: Center(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 8.0,
+                      horizontal: 60.0,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15.0),
+                      gradient: secondContainer(),
+                    ),
+                    child: Text(
+                      "Log In",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account? ",
                     style: TextStyle(
-                        color: Colors.red,
+                      fontSize: 13.0,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.black54,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SignUpPage(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(
                         fontSize: 15.0,
-                        fontWeight: FontWeight.bold),
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
                   ),
                 ],
               ),
-            ),
-            SizedBox(
-              height: 5.0,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: "avi@gmail.com",
-                hintStyle: AppTextStyle.labelStyle(),
-                filled: true,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                  borderSide: BorderSide.none,
-                ),
-                contentPadding: const EdgeInsets.only(
-                  top: 20.0,
-                  left: 16.0,
-                  right: 16.0,
-                  bottom: 8.0,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: " Password ",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 15.0,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  TextSpan(
-                    text: "*",
-                    style: TextStyle(
-                        color: Colors.red,
-                        fontSize: 15.0,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 5.0,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: "Password",
-                hintStyle: AppTextStyle.labelStyle(),
-                filled: true,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                  borderSide: BorderSide.none,
-                ),
-                contentPadding: const EdgeInsets.only(
-                  top: 20.0,
-                  left: 16.0,
-                  right: 16.0,
-                  bottom: 8.0,
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
